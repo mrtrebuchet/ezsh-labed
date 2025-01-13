5c63ab67d39f5e3dc2955f4f57c613e9870f2bef
mrtrebuchet <edwalt222@gmail.com>
2025-01-13 22:29:16 +1100

    first draft of setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 # List of applications to get and install
-applications=("autoconf" "cmake" "cowsay" "curl" "fuse" "git" "make" "nano" "python3" "tmux" "vim" "wget" "zsh")
+coreapps=("autoconf" "cmake" "curl" "fuse" "git" "make" "nano" "python3" "tmux" "vim" "wget" "zsh")
+
 
 # Function to install packages on Debian-based systems
 install_debian() {
-  for app in "${applications[@]}"; do
+  for app in "${coreapps[@]}"; do
     if ! dpkg -l | grep -q "^ii\s*${app} "; then
       echo "${app} is not installed. Installing..."
       sudo apt-get update && sudo apt-get install -y "${app}"
@@ -13,6 +14,7 @@ install_debian() {
       echo "${app} is already installed."
     fi
   done
+  echo "Core applications installed"
 }
 
 # Function to install packages on Arch-based systems
@@ -25,7 +27,30 @@ install_arch() {
       echo "${app} is already installed."
     fi
   done
+  echo "Core applications installed"
 }
+
+# Install and configure shell
+install_shell() {
+  echo "Installing nerd fonts"
+  wget -q --show-progress -N https://github.com/0xType/0xProto/blob/main/fonts/0xProto-Regular.ttf -P ~/.local/share/fonts/
+  wget -q --show-progress -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts/
+  wget -q --show-progress -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts/
+  wget -q --show-progress -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts/
+  wget -q --show-progress -N https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -P ~/.local/share/fonts/
+  echo "installing OhMyZsh"
+  sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  echo "Installing zplug"
+  sudo curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+  echo "Installing PowerLevel10k"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  mv -n ~/.zshrc ~/.zshrc-backup-$(date +"%Y-%m-%d");
+  cp .n ./example.zshrc ~/.zshrc
+  exec zsh
+  p10k configure
+}
+
+#
 # -------------------
 # Start of Script
 # -------------------
@@ -37,10 +62,12 @@ if [ -f /etc/os-release ]; then
     debian|ubuntu)
       echo "Detected Debian-based system."
       install_debian
+      install_shell
       ;;
     arch)
       echo "Detected Arch-based system."
       install_arch
+      install_shell
       ;;
     *)
       echo "Unsupported Linux distribution: $ID"
@@ -51,7 +78,3 @@ else
   echo "No configuration for installed OS."
   exit 1
 fi
-
-
-
-echo cowsay "script done"
